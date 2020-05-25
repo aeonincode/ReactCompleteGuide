d7918894b58e51c18c6d03b993d286413ab93f3e
aeonincode <khashayar.sasannia@hotmail.com>
2020-05-25 12:42:53 +0200

    using shouldComponentUpdate for optimization

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,11 +6,10 @@ const Cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     // Http request...
-    const timer = setTimeout(() => {
+    setTimeout(() => {
       alert('Saved data to cloud');
     }, 1000);
     return () => {
-      clearTimeout(timer);
       console.log('[Cockpit.js] cleanup work in useEffect');
     };
   }, []);
